4bc8882b7d541a8be011285f4926e3a84a415619
Kernc <kerncece@gmail.com>
2013-03-19 09:49:28 +0100

    hangman updated + skeleton

--- html/hangman/skeleton/script.js
+++ html/hangman/skeleton/script.js
@@ -30,7 +30,9 @@ var phrases = [
 ];
 
 function select_random_phrase(phrases) {
-  return phrases[Math.floor(Math.random() * phrases.length)];
+  // How to get random index on integer interval [0, phrases.length - 1]
+  // from Math.random() which returns a real value on [0, 1] ?
+  return phrases[...];  // TODO
 }
 
 // Initialize variables
@@ -40,22 +42,22 @@ var secret, char_boxes, already_used,
 
 function init() {
   // Set secret to random phrase from 'phrases'
-  secret = select_random_phrase(phrases);
+  secret;  // TODO
   // Build HTML string representing char boxes
   var str = '';
   for (var i=0; i<secret.length; ++i)
     // Treat spaces separately (e.g. show by default)
-    if (secret[i] == ' ')
+    if (...)  // TODO
       str += '<span class="space"><b>&nbsp;</b></span>';
     // Display any _invalid_ characters (e.g. punctuation)
     else if (!is_valid_charCode(secret[i].charCodeAt(0)))
       str += '<span class="visible"><b>' + secret[i] + '</b></span>';
-    // Else, add the character, but hidden
+    // Else, add the character, but hidden (managed in CSS)
     else str += '<span><b>' + secret[i] + '</b></span>';
   // Add HTML string into document model
-  document.getElementById('secret-phrase').innerHTML = str;
+  // TODO
   // 'Grab' 'char boxes' into an array
-  char_boxes = document.getElementById('secret-phrase').getElementsByTagName('b');
+  // TODO
   // Save secret string in lower case for easier comparison
   secret = secret.toLowerCase();
   // (Re)set defaults
@@ -70,47 +72,39 @@ function init() {
 function is_valid_charCode(charCode) {
   // Reference decimal values at
   // http://en.wikipedia.org/wiki/ASCII#ASCII_printable_characters
-  return  65 <= charCode && charCode <= 90  ||
-          97 <= charCode && charCode <= 122 ||
-          127 < charCode;
+  return false; // TODO
 }
 window.onkeypress = function (event) {
   var charCode = event.charCode;
   // If charCode is not valid, return
-  if (!is_valid_charCode(charCode)) return;
+  // TODO
   // Convert charCode into printable Unicode character
-  var char = String.fromCharCode(charCode).toLowerCase();
+  var char = String.fromCharCode(event.charCode).toLowerCase();
   // If char 'char' was already considered, return
-  if (char in already_used) return;
+  // TODO
   // Otherwise, mark 'char' as used
-  already_used[char] = true;
+  // TODO
 
   // Test if char is present in the secret phrase...
-  var have_match = false;
-  for (var i=0; i<secret.length; ++i)
-    if (secret[i] == char) {
+  for (...)  // TODO
+    if (...) {  // TODO
       // ...if so, mark its box as visible
       char_boxes[i].style.visibility = 'visible';
-      have_match = true;
     }
 
   // If char WAS present in the secret phrase...
-  if (have_match) {
+  if (...) {  // TODO
     // ...add it to history
-    history.innerHTML += char + ', ';
+    // TODO
     // Check whether all char boxes are now visible (=> Success!)
-    if (is_game_success())
-      return alert('Bravo!');
+    // TODO
   } else {
     // ...otherwise, add it to history striked-through
     history.innerHTML += '<del>' + char + '</del>, ';
     // ...and draw the next hangman
-    gallows.innerHTML = hangman[hangman.current++];
+    // TODO
     // If this is the last hangman, game over
-    if (hangman.current == hangman.length) {
-      alert('Game over!');
-      init();
-    }
+    // TODO
   }
 }
 // === /Event-driven programming ===
@@ -119,11 +113,9 @@ function is_game_success() {
   function is_hidden(element) {
     return window.getComputedStyle(element).getPropertyValue('visibility') != 'visible';
   }
-  // If ALL boxes are visible, game win!
-  for (var i=0; i<char_boxes.length; ++i)
-    if (is_hidden(char_boxes[i]))  // ...otherwise
-      return false;
-  return true;
+  // If ALL char boxes are visible, game win!
+  // TODO
+  return false;
 }
 
 var hangman = [
